GafferHQ/dependencies

LLVM : add Windows configuration

LLVM/config.py
@@ -15,12 +15,34 @@
 		"mkdir build",
 		"cd build &&"
 			" cmake"
-			" -DCMAKE_INSTALL_PREFIX={buildDir}"
-			" -DCMAKE_BUILD_TYPE=Release"
-			" -DLLVM_ENABLE_RTTI=ON"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			" -D LLVM_ENABLE_RTTI=ON"
 			" ..",
-		"cd build && make install -j {jobs}"
+		"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"move ..\\cfe* tools\\clang",
+			"mkdir build",
+			"cd build &&"
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D BUILD_SHARED_LIBS=OFF"
+				" -D LLVM_REQUIRES_RTTI=ON"
+				" -D LLVM_TARGETS_TO_BUILD=\"X86\""
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
+
+		],
+
+	}
 
 }

hradec/gafferDependencies

LLVM/config.py
@@ -17,12 +17,34 @@
 		"mkdir build",
 		"cd build &&"
 			" cmake"
-			" -DCMAKE_INSTALL_PREFIX={buildDir}"
-			" -DCMAKE_BUILD_TYPE=Release"
-			" -DLLVM_ENABLE_RTTI=ON"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+			" -D LLVM_ENABLE_RTTI=ON"
 			" ..",
-		"cd build && make install -j {jobs}"
+		"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
 
 	],
+
+	"platform:windows" : {
+
+		"commands" : [
+
+			"move ..\\clang* tools\\clang",
+			"mkdir build",
+			"cd build &&"
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D BUILD_SHARED_LIBS=OFF"
+				" -D LLVM_REQUIRES_RTTI=ON"
+				" -D LLVM_TARGETS_TO_BUILD=\"X86\""
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+		],
+
+	}
 
 }